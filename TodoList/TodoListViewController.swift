e155f8c1e75d47177e2a39c103e0ca04710b6878
eungu <eungu0920@naver.com>
2021-07-23 14:37:41 +0900

    Text Input View 추가하기

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -27,17 +27,26 @@ class TodoListViewController: UIViewController {
 //        TODO: 데이터 불러오기[O]
         todoListViewModel.loadTasks()
         
-        let todo = TodoManager.shared.createTodo(detail: "👍🚀 Corona 난리", isToday: true)
-        Storage.saveTodo(todo, fileName: "test.json")
+//        let todo = TodoManager.shared.createTodo(detail: "👍🚀 Corona 난리", isToday: true)
+//        Storage.saveTodo(todo, fileName: "test.json")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let todo = Storage.restoreTodo("test.json")
-        print("---> restore from disk: \(todo)")
+//        let todo = Storage.restoreTodo("test.json")
+//        print("---> restore from disk: \(todo)")
     }
-
+    
+    @IBAction func isTodayButtonTapped(_ sender: Any) {
+//        TODO: 투데이 버튼 토글 작업[O]
+        isTodayButton.isSelected = !isTodayButton.isSelected
+    }
+    
+    @IBAction func addTaskButtonTapped(_ sender: Any) {
+//        TOD
+    }
+    
 }
 
 extension TodoListViewController {
